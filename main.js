f824f39262b3a8ffddc3f2e8a9a582b7437bc8e8
Olesia <borutsao@gmail.com>
2025-10-10 16:00:12 +0300

    Add try-catch for file output

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const { Command } = require("commander");
 const fs = require("fs");
 
@@ -14,12 +13,10 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
-
 if (!options.input) {
   console.error("Please, specify input file");
   process.exit(1);
 }
-
 
 if (!fs.existsSync(options.input)) {
   console.error("Cannot find input file");
@@ -35,21 +32,27 @@ try {
   process.exit(1);
 }
 
-
 if (options.normal) {
   data = data.filter(bank => bank.COD_STATE === "1" || bank.COD_STATE === 1);
 }
-
 
 let result = data.map(bank => {
   const name = bank.FULLNAME || bank.SHORTNAME || "Unknown bank";
   return options.mfo ? `${bank.MFO} ${name}` : name;
 });
 
-
 if (options.display) {
   console.log(result.join("\n"));
 }
-if (options.output) {
-  fs.writeFileSync(options.output, result.join("\n"), "utf-8");
+
+if (options.output) {   // обробка помилки запису файлу
+  try {
+    //evvjdyj  папка з тією самою назвою
+    fs.writeFileSync(options.output, result.join("\n"), "utf-8");
+    console.log(`Result successfully saved to: ${options.output}`);
+  } catch (err) {
+    
+    console.error("Error writing to file  :", err.message);
+    process.exit(1);
+  }
 }
